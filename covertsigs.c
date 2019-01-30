b9101e608831144d12e892fae1fe75a8948f2548
Thomas Hryniw <thryniw@cs.ualberta.ca>
2019-01-29 21:22:39 -0700

    double works single started

--- covertsigs.c
+++ covertsigs.c
@@ -7,21 +7,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
-
+#include <sys/time.h>
+	
 #ifdef SINGLE
-	const int MODE = 2;
+	int MODE = 1;
+#else
+	int MODE = 2;
 #endif  
 
 #define BUFFER_SIZE 4096
 void SigHandler(int signo);
-int getLine (char** buffer, int size, FILE* stream);
+int getLine (char* buffer, int size);
 void sendMessage (pid_t pid, char * buffer);
 void installSigactions( int, struct sigaction* );
 
 int counter_usr1 = 0;
 int counter_usr2 = 0;
 int character = 0;
-char * receivedstr;
+int letter;
+int timeSet = 0;
+struct timeval start;
+struct timeval endtime;
+char receivedstr [BUFFER_SIZE];
 
 int main(int argc, char* argv[]) {
 	pid_t PID;
@@ -48,11 +55,10 @@ int main(int argc, char* argv[]) {
 	}
 		
 	while(!end) {
-		if (getLine(&selfBuffer, BUFFER_SIZE, stdin) > 0) {
+		if (getLine(selfBuffer, BUFFER_SIZE) > 0) {
 			sendMessage(oPID, selfBuffer);
 		}
 	}
-	
 	
 	printf("%d\n", oPID);
 	
@@ -60,54 +66,105 @@ int main(int argc, char* argv[]) {
 }
 
 void SigHandler(int signo) {
-	char chr[1];
-	if (signo == SIGUSR1) {
-		character = (character << 1);
-		++counter_usr1;
-	} else if (signo == SIGUSR2) {
-		character = (character << 1) + 1;
-		++counter_usr2;
-	} else {
-		printf("Undefined signal number!\n");
-		exit(1);
+	if (MODE == 2) {
+		if (signo == SIGUSR1) {
+			character = (character << 1) + 1;
+			++counter_usr1;		//adds 1 bit 
+		} else if (signo == SIGUSR2) {
+			character = (character << 1);
+			++counter_usr2;		//adds 0 bit
+		} else {
+			printf("Undefined signal number!\n");
+			exit(1);
+		}
+		
+	} else if (MODE == 1) {
+	  gettimeofday(&endtime, NULL);
+		if (signo == SIGUSR1) {
+			printf("%ld, timeSet: %d\n", endtime.tv_usec - start.tv_usec, timeSet);
+			if(!timeSet){
+				timeSet = 1;
+				gettimeofday(&start, NULL);
+			} else if(endtime.tv_usec - start.tv_usec < 60 && timeSet) {
+				timeSet = 0;
+				printf("Got 1\n");
+				character = (character << 1) + 1;
+				++counter_usr1;
+			} else if (endtime.tv_usec - start.tv_usec >= 60 && timeSet) {
+			  timeSet = 0;
+			  printf("Got 0\n");
+				character = (character << 1);
+				++counter_usr1;
+			}
+		}	
+		
 	}
 	if (counter_usr1 + counter_usr2 == 8) {
 		counter_usr1 = 0;
 		counter_usr2 = 0;
-		itoa(character, chr, 10);
-		strcat(receivedstr, chr);
+		printf("letter at: %d\n", letter);
+		receivedstr[letter++] = character;
+		
 		if(character == 0) {
-			printf("%s", receivedstr);
+			printf("%s\n", receivedstr);
+			letter = 0;
 		}
 		character = 0;
 	}
 	return;
 }
 
-int getLine (char** buffer, int size, FILE* stream){
+int getLine (char* buffer, int size){
   int i;
   for(i = 0;i < size-1;i++){
-  	char c = getc(stream);
+  	char c = getchar();
   	if(c == '\n' || c == '\r') break;
-  	buffer[i] = &c;
+  	if(c != EOF) {
+  		buffer[i] = c;
+  	} else {
+  		i--;
+  	}
   }
-  for (int j = i; j < size; j++) buffer[j] = '\0';
+  buffer[i] = '\0';
   return i;
 }
 
 void sendMessage (pid_t pid, char * buffer) {
-	for(int i; buffer[i] != '\0'; i++) {
+	for(int i = 0; buffer[i] != '\0'; i++) {
+		printf("current letter: %c\n", buffer[i]); 
 		for(int j = 0; j < 8; j++) {
-			if(buffer[i] & (1 << j)) {
-				kill(pid, SIGUSR1);
+			if(MODE == 2){
+				if(buffer[i] & (1 << (7-j))) {
+					kill(pid, SIGUSR1);		//sending 0
+				} else {
+					kill(pid, SIGUSR2);		//sending 1
+				}
+	
 			} else {
-				kill(pid, SIGUSR2);
+				if(buffer[i] & (1 << (7-j))) {
+					kill(pid, SIGUSR1);
+					sleep(0.05);
+					kill(pid, SIGUSR1);
+					printf("Sent 1\n");
+				} else {
+					kill(pid, SIGUSR1);
+					sleep(0.5);
+					kill(pid, SIGUSR1);
+					printf("Sent 0\n");
+				}
 			}
 			sleep(0.05);
 		}
 	}
 	for(int j = 0; j < 8; j++) {
-		kill(pid, SIGUSR1);
+		if (MODE == 2) {
+			kill(pid, SIGUSR2);
+		} else {
+			kill(pid, SIGUSR1);
+			sleep(0.2);
+			kill(pid, SIGUSR1);
+		}
+		sleep(0.05);
 	}
 	return;
 }
